ddef470a69fed366890c5c08821147cd018b107d
Patrascu Hary <harypatrascu.1@gmail.com>
2024-12-30 17:27:14 +0200

    Database setup done with Identity.

---
+++ StudentManagementSystem/Data/AppDatabaseContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using StudentManagementSystem.Models;
+
+namespace StudentManagementSystem.Data
+{
+    public class AppDatabaseContext : IdentityDbContext<User>
+    {
+        #region Constructors
+        public AppDatabaseContext(DbContextOptions options) : base(options)
+        {
+        }
+        #endregion
+    }
+}
---
+++ StudentManagementSystem/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace StudentManagementSystem.Models
+{
+    public class User : IdentityUser
+    {
+        #region Private properties
+        private string _Name;
+        #endregion
+
+        #region Setters & Getters
+        public string Name
+        {
+            get { return _Name; }
+            set { _Name = value; }
+        }
+        #endregion
+    }
+}
