de2ea20da4fd3e7518c4bff116caefc2e0b5d9a3
Patrascu Hary <harypatrascu.1@gmail.com>
2024-12-30 18:46:04 +0200

    Added the registration functionality and stylized the register view.

--- StudentManagementSystem/Controllers/AccountController.cs
+++ StudentManagementSystem/Controllers/AccountController.cs
@@ -1,13 +1,65 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using StudentManagementSystem.Models;
+using StudentManagementSystem.ViewModels;
 
 namespace StudentManagementSystem.Controllers
 {
     public class AccountController : Controller
     {
+        #region Private properties
+        private readonly UserManager<User> _userManager;
+        #endregion
+
+        #region Contructors
+        public AccountController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+        #endregion
+
+        #region Register methods
         [HttpGet]
         public IActionResult Register()
         {
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            // Check if the model binding and form submission is valid
+            if (ModelState.IsValid)
+            {
+                // Create a new user with the form data submitted by the user
+                User user = new User
+                {
+                    Name = model.Name,
+                    Email = model.Email,
+                    UserName = model.Email
+                };
+
+                // Attempt to create the user in the database
+                var registerAttempt = await _userManager.CreateAsync(user, model.Password);
+
+                if (registerAttempt.Succeeded)
+                {
+                    // Redirect the user to the login page if successful
+                    return RedirectToAction("Login", "Account");
+                }
+                else
+                {
+                    // Otherwise, display the errors to the user
+                    foreach (var error in registerAttempt.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                    return View(model);
+                }
+            }
+            // If the model binding and form submission is not valid, return the view with the model
+            return View(model);
+        }
+        #endregion
     }
 }
