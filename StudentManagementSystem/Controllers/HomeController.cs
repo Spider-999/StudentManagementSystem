35158e92bda6e7220228d97d9b97f050cf600121
spider <67486366+Spider-999@users.noreply.github.com>
2025-01-04 19:20:16 +0200

    Revert "Hary"

--- StudentManagementSystem/Controllers/HomeController.cs
+++ StudentManagementSystem/Controllers/HomeController.cs
@@ -1,6 +1,4 @@
 using System.Diagnostics;
-using Microsoft.AspNetCore.Authorization.Infrastructure;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using StudentManagementSystem.Models;
 
@@ -9,38 +7,15 @@ namespace StudentManagementSystem.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        private readonly UserManager<User> _userManager;
 
-        public HomeController(ILogger<HomeController> logger, UserManager<User> userManager)
+        public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
-            _userManager = userManager;
         }
 
-        public async Task<IActionResult> Index()
+        public IActionResult Index()
         {
-            // Get the current user and their role/roles
-            var user = await _userManager.GetUserAsync(User);
-            if (user == null)
-                return View();
-
-            var userRole = await _userManager.GetRolesAsync(user);
-            if (userRole == null)
-                return View();
-
-            // Get the first role of the user and redirect them to the appropriate page
-            switch (userRole.First())
-            {
-                // TODO: Move this to AccountController
-                case "Admin":
-                    return RedirectToAction("Index", "Admin");
-                case "Teacher":
-                    return RedirectToAction("Index", "Teacher");
-                case "Student":
-                    return RedirectToAction("Dashboard", "Student");
-                default:
-                    return View();
-            }
+            return View();
         }
 
         public IActionResult Privacy()
