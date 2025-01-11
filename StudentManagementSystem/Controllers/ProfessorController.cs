999ea0b83e5fa366b8ff164402b7128415d092e3
Spider-999 <harypatrascu.1@gmail.com>
2025-01-11 21:51:19 +0200

    Started working on the professor functionalities. Added a professor controller and views.

---
+++ StudentManagementSystem/Controllers/ProfessorController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using StudentManagementSystem.Data;
+using StudentManagementSystem.Models;
+
+namespace StudentManagementSystem.Controllers
+{
+    [Authorize(Roles ="Professor,Admin")]
+    public class ProfessorController : Controller
+    {
+        #region Private properties
+        private UserManager<User> _userManager;
+        private AppDatabaseContext _context;
+        #endregion
+
+        #region Constructor
+        public ProfessorController(UserManager<User> userManager, AppDatabaseContext context)
+        {
+            _userManager = userManager;
+            _context = context;
+        }
+        #endregion
+
+        public async Task<IActionResult> Index()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            // Return 404 not found if the user wasnt found
+            if (user == null)
+                return NotFound();
+            return View(user);
+        }
+    }
+}
