d86bb0c36eb3c8a70553370e8f5aabd8166749be
Spider-999 <harypatrascu.1@gmail.com>
2025-01-11 22:34:31 +0200

    Professors can now view all students and click a students name and view all of their homework.

--- StudentManagementSystem/Controllers/ProfessorController.cs
+++ StudentManagementSystem/Controllers/ProfessorController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using StudentManagementSystem.Data;
 using StudentManagementSystem.Models;
 
@@ -22,6 +23,7 @@ namespace StudentManagementSystem.Controllers
         }
         #endregion
 
+        [HttpGet]
         public async Task<IActionResult> Index()
         {
             var user = await _userManager.GetUserAsync(User);
@@ -29,6 +31,36 @@ namespace StudentManagementSystem.Controllers
             if (user == null)
                 return NotFound();
             return View(user);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ViewStudents()
+        {
+            // Get all of the students from the database and put them in a list
+            var students = await _context.Students.ToListAsync();
+
+            if(students == null)
+                return NotFound();
+
+            // Pass the student list to the view
+            return View(students);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ViewStudentHomeworks(string studentId)
+        {
+            // Get the student the professor clicked on
+            var student = await _context.Students.FindAsync(studentId);
+            if (student == null)
+                return NotFound();
+
+            // Get the student's homeworks
+            var homeworks = await _context.Homeworks
+                .Where(s => s.StudentId == studentId)
+                .ToListAsync();
+
+            ViewBag.StudentName = student.Name;
+            return View(homeworks);
         }
     }
 }
